4b7cfdb12fa0d17e0964af080b7a679897020f93
NuiS4ncE <nuis4nce@hotmail.com>
2023-07-13 23:10:01 +0300

    created a register and error page with logic

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from .views import homePageView, noteWriteView, loginView
+from .views import homePageView, noteWriteView, loginView, registerView
 
 urlpatterns = [
     path('', homePageView, name='home'),
     path('login/', loginView, name='login'),
     path('notes/', noteWriteView, name='notewrite'),
+    path('register/', registerView, name='registerView')
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -5,20 +5,16 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from .models import Account, Note
 from django.contrib.auth import authenticate, login
+from django.db import IntegrityError
 
 @login_required
 def homePageView(request):
     account = Account.objects.get(user_id=request.user.id)
     accounts = Account.objects.exclude(user=request.user)
     doctor = account.doctor
-    #users = User.objects.all()
-    #account_list = list(accounts)
-    #notes = [account.note for account in accounts]
     notes = Note.objects.filter(user_id=request.user.id)
     print("NOTES: " + str(notes))
-    #doctorstatus = [account.doctor for account in accounts]
     print("DOCTOR: " + str(account.doctor))
-    #print("ACCOUNT_LIST: " + str(account_list))
     print("ACCOUNTS: " + str(list(accounts)))
     return render(request, 'pages/index.html', {'notes': notes, 'accounts': accounts, 'doctor': doctor})
 
@@ -38,6 +34,32 @@ def loginView(request):
     else:
         print("RENDERING LOGIN PAGE AGAIN")
         return render(request, 'pages/login.html')
+    
+def registerView(request):
+    print("REGISTERVIEW CALLED")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        firstName = request.POST['firstName']
+        lastName = request.POST['lastName']
+        user = User.objects.create_user(username=username, email=email, password=password, first_name=firstName, last_name=lastName)
+        user.save()
+        print("CREATED USER")
+        account = Account.objects.create(user=user, doctor=False)
+        account.save()
+        print("CREATED ACCOUNT")
+        try:
+            user = authenticate(username=username, password=password)
+        except IntegrityError as e:
+            if "unique constraint" in e.message:
+                error = "Registration failed. User already exists. " + e.message
+                return render(request, 'pages/error.html', {'error': error})
+        if user is not None:
+            login(request, user)
+            print("LOGIN SUCCESSFUL AFTER REGISTER")
+            return redirect('home')
+    return render(request, 'pages/register.html')
 
 @login_required
 def noteWriteView(request):
@@ -45,8 +67,6 @@ def noteWriteView(request):
         toUser = User.objects.get(username=request.POST.get('to'))
         noteText = request.POST.get('notetext')
         note = Note.objects.create(user=toUser, note=noteText)
-        #request.user.account.note = noteText
-        #request.user.save()
         note.save()
 
     return redirect('home')
